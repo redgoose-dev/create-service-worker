4e056c98e52f31d0a1fd7805ba9f3f2e9b339921
redgoose.eth <scripter@me.com>
2023-05-03 00:03:02 +0900

    타입 조정

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,13 @@
 declare module '@redgoose/create-service-worker' {
-  //
+
+  interface CreateServiceWorkerParams {
+    target: string
+    output: string
+    ignore: object
+  }
+
+  function createServiceWorker(opt: CreateServiceWorkerParams): Promise<string[]>
+
+  export default createServiceWorker
+
 }
